005aba0b42c2bb07400b37819baf9f6e361ba739
syedfaheem27 <syedfaheem2719@gmail.com>
2023-12-09 18:51:32 +0530

    Add stack questions

--- DSA-3/stack/questions.js
+++ DSA-3/stack/questions.js
@@ -1,11 +1,11 @@
-const Stack = require("./stack");
+const { StackI, StackII } = require("./stack");
 
 //PROBLEM-1
 //REVERSE A STRING USING A STACK
 //Faheem - meehaf
 
 function reverseStr(str) {
-  const stringStack = new Stack();
+  const stringStack = new StackI();
 
   for (let i = 0; i < str.length; i++) stringStack.push(str[i]);
   console.log(stringStack);
@@ -16,6 +16,72 @@ function reverseStr(str) {
   console.log(output);
 }
 
-reverseStr("faheem");
+// reverseStr("faheem");
 
 ///////////////////////////////////////////////////
+
+//PROBLEM-2
+//NOT EFFICIENT - SORT A STACK
+
+function sortStack(inputStack) {
+  console.log(inputStack);
+  let temp;
+  let tempStack = new StackI();
+
+  while (inputStack.size() !== 0) {
+    temp = inputStack.pop();
+
+    while (tempStack.size() !== 0 && tempStack.peek() < temp) {
+      inputStack.push(tempStack.pop());
+    }
+    tempStack.push(temp);
+  }
+
+  console.log(tempStack);
+}
+
+const inp = new StackI();
+
+//Not returning the object itself from the push() in Stcak class
+// inp.push(2);
+// inp.push(-1);
+// inp.push(-3);
+// inp.push(2);
+// inp.push(4);
+// inp.push(10);
+
+//If i return the object itself from the push method inside the Stack class,
+//i will be able to chain push() calls
+inp.push(2).push(-1).push(-3).push(2).push(4).push(10);
+
+// sortStack(inp);
+
+////////////////////////////////////////////////////
+//IMPLEMENT TWO STACKS USING THE SAME ARRAY
+//Add a stack of a given size
+const twoStacks = new StackII(12);
+
+// twoStacks.push1(2);
+// twoStacks.push1(3);
+// twoStacks.push1(4);
+// twoStacks.push1(5);
+
+// twoStacks.push2(6);
+// twoStacks.push2(7);
+// twoStacks.push2(8);
+// twoStacks.push2(9);
+
+// twoStacks.push2(6);
+// twoStacks.push2(7);
+// twoStacks.push2(8);
+// twoStacks.push2(9);
+
+// console.log(twoStacks.peek1());
+// console.log(twoStacks.peek2());
+
+// console.log(twoStacks.isEmpty1());
+// console.log(twoStacks.isEmpty2());
+
+// console.log(twoStacks);
+
+//////////////////////////////////////////////
--- DSA-3/stack/stack.js
+++ DSA-3/stack/stack.js
@@ -1,15 +1,22 @@
-class Stack {
+class StackI {
   constructor() {
     this.arr = [];
     this.idx = -1;
   }
 
   push(val) {
+    //simple to understand for others
     this.idx++;
     this.arr[this.idx] = val;
+
+    // this.arr[++this.idx]=val;
+    return this;
   }
 
   pop() {
+    if (this.idx - 1 < -1)
+      return console.log("Can't pop elements from the stack when it's empty");
+
     let popNum = this.arr[this.idx];
     this.idx--;
     return popNum;
@@ -26,8 +33,6 @@ class Stack {
     return this.size() === 0 ? true : false;
   }
 }
-
-module.exports = Stack;
 
 // class Stack {
 //   constructor() {
@@ -55,3 +60,73 @@ module.exports = Stack;
 // }
 
 // module.exports = Stack;
+
+class StackII {
+  constructor(len) {
+    this.arr = Array.from({ length: len });
+    this.in1 = -1;
+    this.in2 = len;
+  }
+  checkOverflow() {
+    return this.in1 + 1 >= this.in2 ? false : true;
+  }
+
+  push1(val) {
+    if (!this.checkOverflow()) return console.log("Stack  is full");
+
+    this.in1++;
+    this.arr[this.in1] = val;
+  }
+
+  peek1() {
+    return this.arr[this.in1];
+  }
+
+  pop1() {
+    if (this.in1 - 1 < -1)
+      return console.log("Can't pop elements from the stack when it's empty");
+
+    let num = this.arr[this.in1];
+    this.in1--;
+    return num;
+  }
+
+  size1() {
+    return this.in1 + 1;
+  }
+
+  isEmpty1() {
+    return this.in1 === -1 ? true : false;
+  }
+
+  push2(val) {
+    if (!this.checkOverflow()) return console.log("Stack 1 is full");
+
+    this.in2--;
+    this.arr[this.in2] = val;
+  }
+
+  peek2() {
+    return this.arr[this.in2];
+  }
+
+  pop2() {
+    if (this.in2 + 1 > this.arr.length)
+      return console.log("Can't pop elements from the stack when it's empty");
+
+    let num = this.arr[this.in2];
+    this.in2++;
+    return num;
+  }
+
+  size2() {
+    return this.arr.length - this.in2;
+  }
+
+  isEmpty2() {
+    return this.in2 === this.arr.length ? true : false;
+  }
+}
+
+exports.StackI = StackI;
+exports.StackII = StackII;
