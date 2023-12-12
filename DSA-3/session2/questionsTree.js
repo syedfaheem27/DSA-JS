aeec64b05cb4fa64c142654992e5c10c34584660
syedfaheem27 <syedfaheem2719@gmail.com>
2023-12-13 02:40:09 +0530

    Add binary tree traversal problems

--- DSA-3/session2/questionsTree.js
+++ DSA-3/session2/questionsTree.js
@@ -39,7 +39,7 @@ root.right.left.parent = root.right;
 root.right.right = new TreeNode(4);
 root.right.right.parent = root.right;
 
-function normalLevelOrderTraversal(root) {
+function normalTraversal(root) {
   let queue = new Queue();
   let res = [];
 
@@ -58,4 +58,42 @@ function normalLevelOrderTraversal(root) {
   return res;
 }
 
-normalLevelOrderTraversal(root);
+// normalTraversal(root);
+
+///////////////////////////////////
+
+function zigzagTraversal(treeRoot) {
+  let queue = new Queue();
+
+  let resArr = [],
+    level = 1;
+
+  queue.add(treeRoot);
+
+  while (!queue.isEmpty()) {
+    let size = queue.size();
+    let temp = [];
+
+    for (let i = 0; i < size; i++) {
+      let frontNode = queue.peek();
+
+      if (frontNode.left) queue.add(frontNode.left);
+
+      if (frontNode.right) queue.add(frontNode.right);
+
+      temp.push(queue.remove().val);
+    }
+
+    if (level % 2 === 0) temp.reverse();
+
+    resArr.push(temp);
+    level++;
+  }
+
+  return resArr;
+}
+
+const strArr = zigzagTraversal(root);
+strArr.forEach((arr) => {
+  console.log(...arr);
+});
