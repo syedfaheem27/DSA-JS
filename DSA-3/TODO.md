dc8b884648552db9cb099b964425ed2a1f295587
syedfaheem27 <syedfaheem2719@gmail.com>
2023-12-18 01:06:46 +0530

    Add methods on Linked List class

--- DSA-3/TODO.md
+++ DSA-3/TODO.md
@@ -8,9 +8,9 @@
 - Expression evaluation
 - Balanced Parentheses problem✅
 - Next Greater Element✅
-- Next Smaller Element
+- Next Smaller Element✅
 - Implement stack using 2 queues✅
-- Implement the min() function using a Stack
+- Implement the min() function using a Stack✅
 - Sort the values in a Stack ✅
 - Find the next day with higher temperature
 - Decode an encoded string (repeating values encoded with a number)
@@ -32,9 +32,9 @@
 ## LinkedLists
 
 - Add 1 to a number represented by a linked list
-- Insert an element into a sorted Circular List
+- Insert an element into a sorted Circular List✅
 - Delete Kth to last element in a Linked List
-- Rotate a Linked List
+- Rotate a Linked List✅
 
 ### Reverse pattern in linked lists(example problems)
 
--- DSA-3/linked-list/linked_list.js
+++ DSA-3/linked-list/linked_list.js
@@ -56,8 +56,102 @@ class SinglyLinkedListI {
     this.length++;
     return this;
   }
-  //implement remove method,getNodeAtIndex method and others
+  // remove method at index
+  removeElement(idx) {
+    if (this.length === 0) return console.log("The Linked List is empty");
+
+    if (idx < 0 || idx >= this.length)
+      return console.log("Enter a valid index");
+
+    if (idx === 0) {
+      let currNode = this.head;
+
+      if (currNode.next === null) this.tail = null;
+
+      this.head = currNode.next;
+      this.length--;
+      return currNode;
+    }
+
+    let prevNode = null,
+      currNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+
+    //if idx is pointing to the tail
+    if (currNode.next === null) {
+      this.tail = prevNode;
+    }
+
+    prevNode.next = currNode.next;
+    this.length--;
+
+    return currNode;
+  }
+
+  //remove last element
+  remove() {
+    if (this.length === 0) return console.log("The Linked List is empty");
+
+    let currNode = this.head;
+
+    if (currNode.next === null) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+
+      return currNode;
+    }
+    let prevNode = null;
+
+    while (currNode.next !== null) {
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+
+    prevNode.next = null;
+    this.tail = prevNode;
+    this.length--;
+
+    return currNode;
+  }
+
+  updateElement(idx, node) {
+    if (idx < 0 || idx >= this.length)
+      return console.log("Enter a valid index");
+
+    let currNode = this.head;
+
+    if (idx === 0) {
+      let tobeUpdated = this.head;
+      if (this.head.next === null) this.tail = node;
+
+      this.head = node;
+      return tobeUpdated;
+    }
+
+    let prevNode = null;
+
+    for (let i = 0; i < idx; i++) {
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+    //if idx is pointing to the tail
+    if (currNode.next === null) this.tail = node;
+
+    prevNode.next = node;
+    node.next = currNode.next;
+
+    return currNode;
+  }
+
+  //TODO:add reverse method
 }
+
+//TODO: Implement all the above methods on a singly linked list with only head given
 
 exports.SinglyLinkedListI = SinglyLinkedListI;
 exports.ListNode = Node;
