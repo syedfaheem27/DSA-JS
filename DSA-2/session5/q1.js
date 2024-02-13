0d13fe269144bd482cb8f5ca23abc87e9373aa5c
syedfaheem27 <syedfaheem2719@gmail.com>
2024-02-14 01:30:02 +0530

    Add question: next greater element - DSA-2

--- DSA-2/session5/q1.js
+++ DSA-2/session5/q1.js
@@ -61,3 +61,62 @@ function waveSortII(arr) {
 }
 
 /*--------------------------*/
+
+//PROBLEM 2
+
+//FIND THE NEXT GREATER ELEMENT
+/*
+PROBLEM DESCRIPTION
+
+Problem Description
+Given a positive integer n, find the smallest integer which has exactly the same digits 
+existing in the integer n and is greater in value than n. If no such positive integer 
+exists, return -1.
+
+Input format
+A 32 bit integer N
+
+Output format
+A single number
+
+*/
+
+//TC O(n) & SC O(1)
+
+function nextGreaterElement(num) {
+  let num_arr = num.toString().split("");
+  let n = num_arr.length;
+
+  let swap_idx;
+  for (let i = n - 1; i > 0; i--)
+    if (+num_arr[i] > +num_arr[i - 1]) {
+      swap_idx = i - 1;
+      break;
+    }
+
+  if (!swap_idx) return -1;
+
+  let break_pt = swap_idx + 1;
+
+  //Now we need to check if there is a number that is closest to
+  //num_arr[swap_idx] and also greater than it
+  //Finding a break point ensures that we are always finding the next greater
+  //number
+
+  for (let i = break_pt; i < n - 1; i++) {
+    if (num_arr[i] >= num_arr[swap_idx]) {
+      break_pt = num_arr[break_pt] > num_arr[i] ? i : break_pt;
+    }
+  }
+
+  //make a swap
+  let temp = num_arr[break_pt];
+  num_arr[break_pt] = num_arr[swap_idx];
+  num_arr[swap_idx] = temp;
+
+  const return_arr = num_arr
+    .slice(0, swap_idx + 1)
+    .concat(num_arr.slice(swap_idx + 1).reverse());
+
+  return +return_arr.join("");
+}
