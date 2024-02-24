55157ba6b74329f2c25768c3051495cc91e7c5a8
syedfaheem27 <syedfaheem2719@gmail.com>
2024-02-25 02:15:13 +0530

    Add 3 questions : binary search

--- DSA-2/session8/q1.js
+++ DSA-2/session8/q1.js
@@ -62,4 +62,125 @@ function binarySearch(input) {
   return -1;
 }
 
-console.log(searchSortedArr([4, 5, 6, 7, 0, 1, 2], 0));
+/*------------------------------------*/
+
+//PROBLEM 2
+
+//FIND THE FIRST ONE
+
+/*
+PROBLEM DESCRIPTION
+
+Given a sorted array consisting of 0s and 1s only, find the 
+index of the first 1. If there’s no 1 present in the array, return -1
+*/
+
+function findFirstOne(arr) {
+  let n = arr.length;
+
+  let l = 0,
+    r = n - 1;
+
+  let res = -1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (arr[mid] === 0) l = mid + 1;
+    else {
+      r = mid - 1;
+      res = mid;
+    }
+  }
+
+  return res;
+}
+
+/*-------------------------*/
+
+//PROBLEM 3
+
+//FIND THE PEAK ELEMENT
+
+/*
+PROBLEM DESCRIPTION
+
+A peak element is an element that is strictly greater than its neighbors. 
+Given an integer array nums that is strictly increasing or decreasing or both, 
+find a peak element, and return its index. If the array contains multiple peaks, 
+return the index to any of the peaks. You may imagine that nums[-1] = nums[n] = -∞. 
+You must write an algorithm that runs in O(log n) time.
+*/
+
+function findPeakEl(arr) {
+  let n = arr.length;
+
+  if (n === 1 || arr[0] > arr[1]) return 0;
+  if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+  let l = 0,
+    r = n - 1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (
+      mid === 0 ||
+      mid === n - 1 ||
+      (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+    )
+      return mid;
+    else if (arr[mid] < arr[mid + 1]) l = mid + 1;
+    else r = mid - 1;
+  }
+}
+
+/*--------------------------*/
+
+//PROBLEM 3
+
+//FIND THE OCCURRENCES OF AN INTEGER
+
+/*
+PROBLEM DESCRIPTION
+
+Given a sorted integer array of length n with possible duplicate elements. 
+Find the number of occurrences of an integer k using binary search.
+*/
+
+function countOccurences(arr, k) {
+  let n = arr.length;
+
+  let l = 0,
+    r = n - 1;
+
+  let left_idx, right_idx;
+
+  //Find the left index
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (arr[mid] < k) l = mid + 1;
+    else if (arr[mid] > k) r = mid - 1;
+    else {
+      left_idx = mid;
+      r = mid - 1;
+    }
+  }
+
+  (l = 0), (r = n - 1);
+
+  //Find the right index
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (arr[mid] < k) l = mid + 1;
+    else if (arr[mid] > k) r = mid - 1;
+    else {
+      right_idx = mid;
+      l = mid + 1;
+    }
+  }
+  return left_idx && right_idx ? right_idx - left_idx + 1 : 0;
+}
