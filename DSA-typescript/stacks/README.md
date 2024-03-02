2b276ee3539c3d8769fb2d4cd7fc2f25cd2073a4
syedfaheem27 <syedfaheem2719@gmail.com>
2024-03-02 19:28:21 +0530

    Add two stack questions : typescript

---
+++ DSA-typescript/stacks/README.md
@@ -0,0 +1,4 @@
+# Problems covered
+
+1. Find the next greater element in an array
+2. Compare Strings
---
+++ DSA-typescript/stacks/pre-req.js
@@ -0,0 +1,83 @@
+"use strict";
+//Create a stack which always stores the
+//numbers in ascending order lowest at the bottom
+// and highest at the top
+//Add - TC O(n) & SC O(n)
+//Remove - TC O(1) & SC O(1)
+class SortedStack {
+  constructor() {
+    this.stack = [];
+  }
+  add(el) {
+    if (this.stack[this.length - 1] < el || this.isEmpty) {
+      this.stack.push(el);
+      return this;
+    }
+    let temp_stack = [];
+    while (!this.isEmpty && this.stack[this.length - 1] > el) {
+      let num = this.stack.pop();
+      if (num !== undefined) temp_stack.push(num);
+      else break;
+    }
+    this.stack.push(el);
+    while (temp_stack.length !== 0) {
+      let num = temp_stack.pop();
+      if (num !== undefined) this.stack.push(num);
+    }
+    return this;
+  }
+  remove() {
+    if (this.isEmpty) {
+      console.log("Can't remove elements from an empty stack");
+      return;
+    }
+    let num = this.stack.pop();
+    return num;
+  }
+  get isEmpty() {
+    return this.stack.length === 0 ? true : false;
+  }
+  get length() {
+    return this.stack.length;
+  }
+}
+/*------------------------------_*/
+//Given a stack of integers, sort it in ascending order using another temporary stack.
+/*
+Input : [34, 3, 31, 98, 92, 23]
+Output : [3, 23, 31, 34, 92, 98]
+
+Input : [3, 5, 1, 4, 2, 8]
+Output : [1, 2, 3, 4, 5, 8]
+*/
+
+//TC O(n2) & SC O(n)
+function sortStack(stack) {
+  let temp_stack = [];
+  while (stack.length !== 0) {
+    let num = stack.pop();
+    if (num !== undefined) {
+      if (temp_stack.length === 0) {
+        temp_stack.push(num);
+        continue;
+      }
+      let count = 0;
+      while (temp_stack[temp_stack.length - 1] > num) {
+        let top_num = temp_stack.pop();
+        if (top_num !== undefined) {
+          stack.push(top_num);
+          count++;
+        }
+      }
+      temp_stack.push(num);
+      while (count !== 0) {
+        let new_num = stack.pop();
+        if (new_num !== undefined) {
+          temp_stack.push(new_num);
+          count--;
+        }
+      }
+    }
+  }
+  return temp_stack;
+}
