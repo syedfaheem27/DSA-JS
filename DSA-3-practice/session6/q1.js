eee150958777be898452af75db19be8fb98addc2
syedfaheem27 <syedfaheem2719@gmail.com>
2024-03-14 00:01:04 +0530

    Add question : sort a linked list

--- DSA-3-practice/session6/q1.js
+++ DSA-3-practice/session6/q1.js
@@ -1,3 +1,5 @@
+const { Node } = require("../linkedlist/l1");
+
 //PROBLEM 1 : GIVEN A LINKED LIST - DETERMINE IF IT HAS A CYCLE IN IT
 
 /*
@@ -81,3 +83,78 @@ function removeCycle(head) {
 }
 
 /*---------------------------------------------*/
+
+//PROBLEM 3 : SORT A LINKED LIST IN O(NlogN)
+
+//Using merge sort
+
+//TODO: Find out why this piece of code gives us a segmentation fault
+//and when we change the fast=head.next in getMidNode, it works fine
+function getMidNode(head) {
+  let fast = head,
+    slow = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+}
+
+function mergeSortedList(head1, head2) {
+  let new_head = new Node(-1);
+  let tail = new_head;
+
+  while (head1 !== null && head2 !== null) {
+    if (head1.val <= head2.val) {
+      tail.next = head1;
+      head1 = head1.next;
+    } else {
+      tail.next = head2;
+      head2 = head2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  while (head1 !== null) {
+    tail.next = head1;
+    tail = tail.next;
+    head1 = head1.next;
+  }
+
+  while (head2 !== null) {
+    tail.next = head2;
+    tail = tail.next;
+    head2 = head2.next;
+  }
+
+  return new_head.next;
+}
+
+function sortLinkedList(head) {
+  if (head === null || head.next === null) return head;
+
+  const mid = getMidNode(head);
+  let left_node = head;
+  let right_node = mid.next;
+
+  //destroying the middle link
+  mid.next = null;
+
+  let l1 = sortLinkedList(left_node);
+  let l2 = sortLinkedList(right_node);
+
+  return mergeSortedList(l1, l2);
+}
+
+const n1 = new Node(-2);
+const n2 = new Node(1);
+const n3 = new Node(6);
+const n4 = new Node(-4);
+const n5 = new Node(4);
+n1.next = n2;
+n2.next = n3;
+n4.next = n5;
+// console.log(sortLinkedList(n1));
