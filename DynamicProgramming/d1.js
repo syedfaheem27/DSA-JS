a842ae755815f244431ea2c31606052b80138543
syedfaheem27 <syedfaheem2719@gmail.com>
2024-04-30 18:05:16 +0530

    Add climbing stairs - dp

--- DynamicProgramming/d1.js
+++ DynamicProgramming/d1.js
@@ -62,3 +62,93 @@ function findFibII(n) {
 }
 
 /*-------------------------------------------*/
+
+// Climbing Stairs
+
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1, 2 or 3 steps. In how many distinct ways can you climb to the top?
+*/
+
+// Memoization - Top Down approach
+//TC O(n) & SC O(n)
+
+/*
+Approach
+
+At each step, i am looking if know the possible number of ways to reach the nth step.
+If yes - get me that from the array
+If no - calculate it recursively
+*/
+
+function climbingStairsI(n) {
+  let waysToClimb = [];
+
+  calculateSteps(0, n, waysToClimb);
+  return waysToClimb[0];
+}
+
+function calculateSteps(pos, n, waysToClimb) {
+  if (pos === n) {
+    return 1;
+  }
+
+  if (pos > n) return 0;
+
+  if (waysToClimb[pos] !== undefined) return waysToClimb[pos];
+
+  waysToClimb[pos] =
+    calculateSteps(pos + 1, n, waysToClimb) +
+    calculateSteps(pos + 2, n, waysToClimb);
+
+  return waysToClimb[pos];
+}
+
+//Tabulation (Bottom up approach)
+
+/*
+Approach
+
+The way to look at the problem  is slightly different here.
+So at each step we're looking at the possible ways to reach that particular step
+So, for 0th steo, there's 1 way and for the 1st step also, there's 1 way to reach it
+*/
+
+//TC O(n) & SC O(n)
+
+function climbingStairsII(n) {
+  if (n <= 1) return 1;
+
+  let waysToReachStep = [];
+
+  waysToReachStep[0] = 1;
+  waysToReachStep[1] = 1;
+
+  for (let i = 2; i <= n; i++)
+    waysToReachStep[i] = waysToReachStep[i - 1] + waysToReachStep[i - 2];
+
+  return waysToReachStep[n];
+}
+
+//Optimising the tabulation approach
+
+//SC O(1) & TC O(n)
+
+function climbingStairsIII(n) {
+  if (n <= 1) return 1;
+
+  let prev_prev_ways = 1; //Ways to reach 0th step
+  let prev_ways = 1; //ways to reach 1st step
+
+  let curr_ways;
+
+  for (let i = 2; i <= n; i++) {
+    curr_ways = prev_prev_ways + prev_ways;
+
+    prev_prev_ways = prev_ways;
+    prev_ways = curr_ways;
+  }
+
+  return curr_ways;
+}
