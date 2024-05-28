3357d0730b574703a7d526383d60b9a72cfa225a
syedfaheem27 <syedfaheem2719@gmail.com>
2024-05-28 18:58:52 +0530

    Add minimum window substring

--- Neet_Code/SlidingWindow/q1.js
+++ Neet_Code/SlidingWindow/q1.js
@@ -37,3 +37,57 @@ function distinctIntegers(nums, k) {
 
   return count;
 }
+
+/*-------------------------------------*/
+
+//HARD: Minimum Window Substring
+
+/*
+Given two strings s and t, return the shortest substring of s such that 
+every character in t, including duplicates, is present in the substring. 
+If such a substring does not exist, return an empty string "".
+
+You may assume that the correct output is always unique.
+*/
+
+// TC O(N) && SC O(1)
+function minWindow(s, t) {
+  let map = new Map();
+
+  for (let i = 0; i < t.length; i++) map.set(t[i], map.get(t[i]) + 1 || 1);
+
+  let start = 0;
+  let end = 0;
+  let n = s.length;
+
+  let frequency = 0;
+  let len = Number.MAX_SAFE_INTEGER;
+
+  let res_start = -1;
+
+  let freq_map = new Map();
+
+  while (end < n && start <= end) {
+    freq_map.set(s[end], freq_map.get(s[end]) + 1 || 1);
+
+    if (map.has(s[end]) && map.get(s[end]) === freq_map.get(s[end]))
+      frequency++;
+
+    while (start <= end && frequency === map.size) {
+      if (len >= end - start + 1) {
+        res_start = start;
+        len = end - start + 1;
+      }
+
+      freq_map.set(s[start], freq_map.get(s[start]) - 1);
+
+      if (map.has(s[start]) && freq_map.get(s[start]) !== map.get(s[start]))
+        frequency--;
+
+      start++;
+    }
+
+    end++;
+  }
+  return res_start !== -1 ? s.slice(res_start, res_start + len) : "";
+}
